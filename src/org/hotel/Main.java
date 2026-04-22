1354e796a6ff1b438fbae828c24673c12248dced
besartsinani <besartsinan@gmail.com>
2026-04-22 15:23:59 +0200

    Implement Main

---
+++ src/org/hotel/Main.java
@@ -0,0 +1,43 @@
+package org.hotel;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Hotel hotel = new Hotel("Hilton Hotel");
+
+        Room r1 = new Room("701", "Standard", 50.00);
+        Room r2 = new Room("702", "Deluxe", 100.00);
+        Room r3 = new Room("703", "Suite", 200.00);
+        hotel.addRoom(r1);
+        hotel.addRoom(r2);
+        hotel.addRoom(r3);
+
+        Guest g1 = new Guest("G1", "Besart", "Sinani", "besartsinan@gmail.com");
+        Guest g2 = new Guest("G2", "Meriton", "Ademi", "meritonademi@gmail.com");
+        Guest g3 = new Guest("G3", "Ali", "Amzai", "alaamz@gmail.com");
+
+        hotel.makeBooking("B1", r1, g1,
+                LocalDate.of(2026, 4, 22),
+                LocalDate.of(2026, 4, 27));
+
+        hotel.makeBooking("B2", r2, g2,
+                LocalDate.of(2026, 4, 15),
+                LocalDate.of(2026, 4, 20));
+
+        hotel.makeBooking("B3", r1, g3,
+                LocalDate.of(2026, 4, 23),
+                LocalDate.of(2026, 4, 25));
+
+        hotel.cancelBooking("B1");
+
+        hotel.makeBooking("B4", r1, g3,
+                LocalDate.of(2026, 4, 22),
+                LocalDate.of(2026, 4, 27));
+
+        hotel.displayRooms();
+        hotel.displayBookings();
+
+    }
+}
