d32755accf6fe0dbab2c2e1a8324dae11f606c26
besartsinani <besartsinan@gmail.com>
2026-04-25 21:51:41 +0200

    feat: add handleComplaints to FrontDeskStaff (task 9)

--- src/org/hotel/FrontDeskStaff.java
+++ src/org/hotel/FrontDeskStaff.java
@@ -18,6 +18,27 @@ public class FrontDeskStaff extends Staff {
         System.out.println("Hello " + guestName + "! Welcome to the hotel. My name is " + name + ".");
     }
 
+    public void handleComplaints(String[] complaints) {
+
+        for (int i = 0; i < complaints.length; i++){
+
+        switch (complaints[i]) {
+            case "wifi":
+                System.out.println("IT team notified.");
+                break;
+            case "noise":
+                System.out.println("Security notified.");
+                break;
+            case "cleanliness":
+                System.out.println("Housekeeping team notified.");
+                break;
+            default:
+                System.out.println("Manager notified.");
+        }
+
+        }
+    }
+
     @Override
     public String toString() {
         return "FrontDeskStaff{" +
